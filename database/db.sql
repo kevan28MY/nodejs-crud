da68f53114a03b268d60913d49b083561d7c2f2d
Melgarejo <kevinmelgarejoyuyali28@gmail.com>
2024-10-27 02:19:36 -0500

    crud with nodejs and postgresql

---
+++ database/db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email) VALUES ('Jhon Doe', 'jhon@email.com');
+
+SELECT * FROM users;
